de55b987e8960b3105e544707e05ca396fe866b7
stefanprodan <stefan.prodan@gmail.com>
2020-04-06 10:43:06 +0300

    Implement Git sync with go-git

--- controllers/gitrepository_controller.go
+++ controllers/gitrepository_controller.go
@@ -18,13 +18,21 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
-	sourcerv1alpha1 "github.com/fluxcd/sourcer/api/v1alpha1"
+	sourcerv1 "github.com/fluxcd/sourcer/api/v1alpha1"
 )
 
 // GitRepositoryReconciler reconciles a GitRepository object
@@ -38,16 +46,103 @@ type GitRepositoryReconciler struct {
 // +kubebuilder:rbac:groups=sourcer.fluxcd.io,resources=gitrepositories/status,verbs=get;update;patch
 
 func (r *GitRepositoryReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("gitrepository", req.NamespacedName)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
 
-	// your logic here
+	log := r.Log.WithValues("gitrepository", req.NamespacedName)
 
-	return ctrl.Result{}, nil
+	var repo sourcerv1.GitRepository
+	if err := r.Get(ctx, req.NamespacedName, &repo); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	result := ctrl.Result{RequeueAfter: repo.Spec.Interval.Duration}
+
+	// set initial status
+	if len(repo.Status.Conditions) == 0 {
+		repo.Status.Conditions = []sourcerv1.RepositoryCondition{
+			{
+				Type:   sourcerv1.RepositoryConditionReady,
+				Status: corev1.ConditionUnknown,
+			},
+		}
+		if err := r.Status().Update(ctx, &repo); err != nil {
+			log.Error(err, "unable to update GitRepository status")
+			return result, err
+		}
+	}
+
+	// try git clone
+	readyCondition, err := r.sync(repo.Spec)
+	if err != nil {
+		log.Info("Repository sync failed", "error", err.Error())
+	} else {
+		log.Info("Repository sync succeeded", "msg", readyCondition.Message)
+	}
+
+	// update status
+	timeNew := metav1.Now()
+	readyCondition.LastTransitionTime = &timeNew
+	repo.Status.Conditions = []sourcerv1.RepositoryCondition{readyCondition}
+
+	if err := r.Status().Update(ctx, &repo); err != nil {
+		log.Error(err, "unable to update GitRepository status")
+		return result, err
+	}
+
+	// requeue repository
+	return result, nil
 }
 
 func (r *GitRepositoryReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&sourcerv1alpha1.GitRepository{}).
+		For(&sourcerv1.GitRepository{}).
+		WithEventFilter(predicate.GenerationChangedPredicate{}).
 		Complete(r)
+}
+
+func (r *GitRepositoryReconciler) sync(spec sourcerv1.GitRepositorySpec) (sourcerv1.RepositoryCondition, error) {
+	// determine ref
+	refName := plumbing.NewBranchReferenceName("master")
+	if spec.Branch != "" {
+		refName = plumbing.NewBranchReferenceName(spec.Branch)
+	}
+	if spec.Tag != "" {
+		refName = plumbing.NewTagReferenceName(spec.Tag)
+	}
+
+	// clone
+	repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL:           spec.Url,
+		Depth:         2,
+		ReferenceName: refName,
+	})
+	if err != nil {
+		ex := fmt.Errorf("git clone error %w", err)
+		return sourcerv1.RepositoryCondition{
+			Type:    sourcerv1.RepositoryConditionReady,
+			Status:  corev1.ConditionFalse,
+			Reason:  "GitCloneFailed",
+			Message: ex.Error(),
+		}, ex
+	}
+
+	// read commit hash
+	ref, err := repo.Head()
+	if err != nil {
+		ex := fmt.Errorf("git resolve HEAD error %w", err)
+		return sourcerv1.RepositoryCondition{
+			Type:    sourcerv1.RepositoryConditionReady,
+			Status:  corev1.ConditionFalse,
+			Reason:  "GitCommandFailed",
+			Message: ex.Error(),
+		}, ex
+	}
+
+	return sourcerv1.RepositoryCondition{
+		Type:    sourcerv1.RepositoryConditionReady,
+		Status:  corev1.ConditionTrue,
+		Reason:  "GitCloneSucceed",
+		Message: fmt.Sprintf("commit hash %s", ref.Hash().String()),
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/fluxcd/sourcer
 go 1.13
 
 require (
+	github.com/go-git/go-git/v5 v5.0.0
 	github.com/go-logr/logr v0.1.0
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.8.1
