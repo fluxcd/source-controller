f5cb441a82973e906ade69523c6024d2ffdae293
Hidde Beydals <hello@hidde.co>
2021-07-23 18:15:38 +0200

    Take relative paths in account for Bucket revision
    This commit changes the checksum method which is used to calculate the
    revision of a Bucket source, so that the file paths are taken into
    account and directory structure changes can be observed.
    
    Signed-off-by: Hidde Beydals <hello@hidde.co>

--- controllers/bucket_controller.go
+++ controllers/bucket_controller.go
@@ -356,9 +356,12 @@ func (r *BucketReconciler) auth(ctx context.Context, bucket sourcev1.Bucket) (*m
 	return minio.New(bucket.Spec.Endpoint, &opt)
 }
 
+// checksum calculates the SHA1 checksum of the given root directory.
+// It traverses the given root directory and calculates the checksum for any found file, and returns the SHA1 sum of the
+// list with relative file paths and their checksums.
 func (r *BucketReconciler) checksum(root string) (string, error) {
-	checksum := ""
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	sum := sha1.New()
+	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -369,14 +372,16 @@ func (r *BucketReconciler) checksum(root string) (string, error) {
 		if err != nil {
 			return err
 		}
-		checksum += fmt.Sprintf("%x", sha1.Sum(data))
+		relPath, err := filepath.Rel(root, path)
+		if err != nil {
+			return err
+		}
+		sum.Write([]byte(fmt.Sprintf("%x  %s\n", sha1.Sum(data), relPath)))
 		return nil
-	})
-	if err != nil {
+	}); err != nil {
 		return "", err
 	}
-
-	return fmt.Sprintf("%x", sha1.Sum([]byte(checksum))), nil
+	return fmt.Sprintf("%x", sum.Sum(nil)), nil
 }
 
 // resetStatus returns a modified v1beta1.Bucket and a boolean indicating
